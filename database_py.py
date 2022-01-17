2aa9bbce85f7e8d4823033474691429f92314d83
SajadRahimi1 <sajadrahimi6633@gmail.com>
2022-01-17 15:23:02 +0330

    some changes applied

--- database_py.py
+++ database_py.py
@@ -2,7 +2,8 @@ import sqlite3
 
 
 def select():
-    connection = sqlite3.connect("database.db")
+    connection = sqlite3.connect(
+        "database.db")
     cursor = connection.cursor()
     rows = cursor.execute(
         "SELECT analyze from Analysis ORDER BY date DESC").fetchone()
@@ -11,9 +12,8 @@ def select():
 
 
 def insert(datTime, analyze):
-    connection = sqlite3.connect("database.db")
+    connection = sqlite3.connect(
+        "database.db")
     cursor = connection.cursor()
     cursor.execute(f"INSERT INTO Analysis VALUES ('{datTime}', '{analyze}')")
     connection.commit()
-
-
