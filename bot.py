7d4d2f72fe5f1a0d9253dffdd688fa98df94ba8f
Sajad Rahimi <75030204+SajadRahimi1@users.noreply.github.com>
2022-05-21 23:25:10 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,70 +1,47 @@
-from datetime import datetime
 import logging
-import database_py
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+from telegram import InlineKeyboardMarkup
+from telegram.ext import Updater, CallbackQueryHandler, MessageHandler, Filters
 
+from vid_utils import Video, BadLink
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+def get_format(bot, update):
+    logger.info("from {}: {}".format(update.message.chat_id, update.message.text)) # "history"
+
+    try:
+        video = Video(update.message.text, init_keyboard=True)
+    except BadLink:
+        update.message.reply_text("Bad link")
+    else:
+        reply_markup = InlineKeyboardMarkup(video.keyboard)
+        update.message.reply_text('Choose format:', reply_markup=reply_markup)
 
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+def download_choosen_format(bot, update):
+    query = update.callback_query
+    resolution_code, link = query.data.split(' ', 1)
+    
+    bot.edit_message_text(text="Downloading...",
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+    
+    video = Video(link)
+    video.download(resolution_code)
+    
+    with video.send() as files:
+        for f in files:
+            bot.send_document(chat_id=query.message.chat_id, document=open(f, 'rb'))
 
 
-def update(update, context):
-    new = str(update.message.text).replace("/updateS ", "")
-    print(new)
-    database_py.insert(datetime.now(), new)
-    update.message.reply_text(database_py.select())
-    logger.warning(database_py.select())
+updater = Updater("5028788079:AAHQj5uFxgDX12H80jSWrB6kG-dCHBBwuEE", use_context=True)
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, get_format))
+updater.dispatcher.add_handler(CallbackQueryHandler(download_choosen_format))
 
 
-def analyze(update, context):
-    update.message.reply_text(database_py.select())
-    logger.warning(database_py.select())
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-    logger.warning(database_py.all())
-
-
-def error(update, context):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-
-def main():
-    updater = Updater(
-        "5028788079:AAHQj5uFxgDX12H80jSWrB6kG-dCHBBwuEE", use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("updateS", update))
-    dp.add_handler(CommandHandler("analyze", analyze))
-
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
+updater.idle()
